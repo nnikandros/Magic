c840ca20a2fd17a5a80971df717e05de85055112
nnikandros <58775214+nnikandros@users.noreply.github.com>
2022-07-03 11:57:12 +0200

    Add card data and cards.scv

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import pandas as pd
+from pprint import pprint
+import json
+
 class card:
      def __init__(self, name, mana_cost, power,toughness,colour, cardtype, creaturetype, oracle):
             self.name = name
@@ -10,35 +14,48 @@ class card:
             self.oracle=oracle
 
 Vadrik=card("Vadrik, Astral Archmage", 3, 1, 2, "RU","Legenrary Creature", "Human Wizard", "If it’s neither day nor night, it becomes day as Vadrik, Astral Archmage enters the battlefield. Instant and sorcery spells you cast cost {X} less to cast, where X is Vadrik’s power.Whenever day becomes night or night becomes day, put a +1/+1 counter on Vadrik"  )
-#print(Vadrik.name)
-
-#print(Vadrik.oracle)
-
-class player:
-        def __init__(self,name,healthpoints):
-                self.name=name
-                self.healthpoints=healthpoints
-        # def addhealthpoints(self):
-        #         self.healthpoints=self.healthpoints+1
-        def addhealthpoints(self,number):
-                self.healthpoints=self.healthpoints+ number
-
-
-
-
-
-Nikitas=player("Nikitas", 20)
-
-
-Nikitas.addhealthpoints(5)
-print(Nikitas.healthpoints)
-print(type(Nikitas.healthpoints))
-# print("Nikitas health points are :"); print(Nikitas.healthpoints)
-
-# Nikitas.addhealthpoints()
+# #print(Vadrik.name)
+#
+# #print(Vadrik.oracle)
+#
+# class player:
+#         def __init__(self,name, healthpoints):
+#                 self.name=name
+#                 self.healthpoints=healthpoints
+#         # def addhealthpoints(self):
+#         #         self.healthpoints=self.healthpoints+1
+#         def addhealthpoints(self,number):
+#                 self.healthpoints=self.healthpoints+ number
+#
+# # class hand:
+# #     def __init__(self, number,size ):
+# #         self.number=number
+# #         self.size=size
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# Nikitas=player("Nikitas", 20)
+#
+#
+# Nikitas.addhealthpoints(5)
 # print(Nikitas.healthpoints)
-#print(Nikitas.addhealthpoints())
-# Alexander=player("Alexander",20)
+# print(type(Nikitas.healthpoints))
+# # print("Nikitas health points are :"); print(Nikitas.healthpoints)
+#
+# # Nikitas.addhealthpoints()
+# # print(Nikitas.healthpoints)
+# #print(Nikitas.addhealthpoints())
+# # Alexander=player("Alexander",20)
 #
 # print(Nikitas.healthpoints)
 # print(Alexander.name) ; print(Alexander.healthpoints)
@@ -54,3 +71,29 @@ print(type(Nikitas.healthpoints))
 # Nikitas.name
 # print(Nikitas.name)
 # print(Nikitas.age)
+
+#df=pd.read_json("Azorius_GK2.json")
+
+ #print(df)
+# f = open("Azorius_GK2.json")
+# json_file = json.load(f)
+# data = json.load("Azorius_GK2.json")
+
+df=pd.read_csv("cards.csv")
+# print(df)
+
+# print(df.iloc[:,3])
+# print(df.columns)
+# print(df['colors'])
+
+print(df.columns)
+# print((df.iloc[:5]).to_str())
+
+Vadrik=card("Vadrik, Astral Archmage", 3, 1, 2, "RU","Legenrary Creature", "Human Wizard", "If it’s neither day nor night, it becomes day as Vadrik, Astral Archmage enters the battlefield. Instant and sorcery spells you cast cost {X} less to cast, where X is Vadrik’s power.Whenever day becomes night or night becomes day, put a +1/+1 counter on Vadrik"  )
+
+rownumber=4
+row=df.iloc[4]
+print(row)
+color=df.loc[rownumber,"colors"]
+print(color)
+
