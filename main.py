13c9153fae5b44c40822abe118211ff9cc072500
nnikandros <58775214+nnikandros@users.noreply.github.com>
2022-07-17 20:56:17 +0200

    finish card class

--- main.py
+++ main.py
@@ -6,35 +6,53 @@ dfset0 = pd.read_csv("set0.csv")
 
 
 class card:
-       def __init__(self, name,  manaCost ): # , cmc, colorIdentity, artist, number, type,
-     #   text, printings, flavor, layout, multiverseid, power,toughness, rarity, subtypes, types):
+       def __init__(self, name,manaCost,cmc,colorIdentity ,typeline,text, power,toughness,types,subtypes): # , artist,, ,, rarity): #, ):
            self.name = name
-           self.manaCost =manaCost
-            # self.cmc = cmc
-            # self.colorIdentity = colorIdentity
-            # self.artist = artist
-            # self.number = number
-            # self.type = type
-            # self.text = text
-            # self.printings = printings
-            # self.flavor = flavor
+           self.manaCost = manaCost
+           self.cmc = cmc
+           self.colorIdentity = colorIdentity
+           # self.artist = artist
+           self.typeline = typeline
+           self.text = text
+           self.power = power
+           self.toughness = toughness
+           # self.rarity=rarity
+           self.types = types
+           self.subtypes = subtypes
+              # self.flavor = flavor
             # self.layout = layout
-            # self.multiverseid = multiverseid
-            # self.power = power
-            # self.toughness = toughness
-            # self.rarity=rarity
-            # self.subtypes=subtypes
-            # self.types=types
+             #self.multiverseid = multiverseid
+           #self.printings = printings
 
 
 
 
-name=dfset0.iloc[1]["name"] # same as dfset0[1].name
-manaCost=dfset0.iloc[1].manaCost
-testobject=card(name,manaCost)
+name = dfset0.iloc[1]["name"]
+manaCost = dfset0.iloc[1]["manaCost"]
+cmc = dfset0.iloc[1]["cmc"]
+colorIdentity = dfset0.iloc[1]["colorIdentity"]
+# artist = dfset0.iloc[1]["artist"]
+typeline = dfset0.iloc[1]["type"]
+text=dfset0.iloc[1]["text"]
+power=dfset0.iloc[1]["power"]
+toughness=dfset0.iloc[1]["toughness"]
+types = dfset0.iloc[1]["types"]
+subtypes = dfset0.iloc[1]["subtypes"]
+
+
+testobject = card(name,manaCost,cmc,colorIdentity,typeline,text,power,toughness,types,subtypes)
+
+
 print(testobject.name)
 print(testobject.manaCost)
-
+print(testobject.cmc)
+print(testobject.typeline)
+print(testobject.colorIdentity)
+print(testobject.text)
+print(testobject.power)
+print(testobject.toughness)
+print(testobject.types)
+print(testobject.subtypes)
 
 
 
